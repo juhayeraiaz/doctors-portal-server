0cc587ba33a1eedaaa035543dde01e124aa205a3
juhayerayaz <aiazj97@gmail.com>
2022-05-15 23:25:05 +0600

    user collection database connected

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ async function run() {
     await client.connect();
     const serviceCollection = client.db('doctors_portal').collection('services');
     const bookingCollection = client.db('doctors_portal').collection('bookings');
+    const userCollection = client.db('doctors_portal').collection('users');
 
     app.get('/service', async (req, res) => {
       const query = {};
@@ -27,6 +28,19 @@ async function run() {
       const services = await cursor.toArray();
       res.send(services);
     });
+
+    app.put('/user/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updatedDoc, options);
+      res.send(result);
+    })
+
 
     app.get('/available', async (req, res) => {
       const date = req.query.date;
@@ -51,6 +65,13 @@ async function run() {
 
       res.send(services)
 
+    })
+
+    app.get('/booking', async (req, res) => {
+      const patient = req.query.patient;
+      const query = { patient: patient };
+      const bookings = await bookingCollection.find(query).toArray();
+      res.send(bookings);
     })
 
     app.post('/booking', async (req, res) => {
